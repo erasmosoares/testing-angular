175642cd770316e99ac51747adc8d40f76b1cd24
Erasmo Soares <esaj@cesar.org.br>
2019-03-11 13:30:30 -0300

    Setup and Tear Down

--- src/app/03-setup-and-teardown/vote.component.spec.ts
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,37 @@
 import { VoteComponent } from './vote.component'; 
 
 describe('VoteComponent', () => {
-  it('', () => {
+
+  let component: VoteComponent;
+
+  beforeEach(()=>{
+    //Arrange
+    component = new VoteComponent();
   });
 
-  it('', () => {
+  afterEach(()=>{
+
+  });
+
+  beforeAll(()=>{
+
+  });
+
+  afterAll(()=>{
+
+  });
+
+  it('should increment totalVotes when upvoted', () => {
+    //Act
+    component.upVote();
+    //Assert
+    expect(component.totalVotes).toBe(1);
+  });
+
+  it('should decrement totalVotes when downvotes', () => {
+       
+    component.downVote();
+    
+    expect(component.totalVotes).toBe(-1);
   });
 });
